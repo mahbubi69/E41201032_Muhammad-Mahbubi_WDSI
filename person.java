e5ba2471ec291078cd8721b131d1197cd65e5a5b
mahbubi69 <80621275+mahbubi69@users.noreply.github.com>
2021-03-16 23:30:23 +0700

    Add files via upload

---
+++ person.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person;
+
+/**
+ *
+ * @author soffid.ix
+ */
+public class person {
+    public static void main(String[]args){
+        String fName = "Muhammad Mahbubi";
+        String stuId = "E41201032";
+        String stuStatus = "Active";
+        
+        System.out.println("Nama    = " +fName);
+        System.out.println("NIM     = " +stuId);
+        System.out.println("Status  = " +stuStatus);
+    }
+}
---
+++ problemvariable.java
@@ -0,0 +1,23 @@
+
+package person;
+
+
+public class problemvariable {
+ public static void main(String[] args) {
+        
+        //deklarasi variabel yang benar
+        boolean gameOver = false;
+        int students=50, classes =3;
+        double sales_tax;
+        short number1;
+        
+        //deklarasi variabel yang salah
+        int 2beOrNot2be;
+        float price index;
+        double lastYear'sPrice;
+        long class;
+                
+    }
+}
+}   
+}
